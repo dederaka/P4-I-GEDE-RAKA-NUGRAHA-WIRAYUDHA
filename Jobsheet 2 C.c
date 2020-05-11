8a5f9c0ae4026072f63d8a8f9fc6df1281755d78
dederaka <65152409+dederaka@users.noreply.github.com>
2020-05-11 21:12:06 +0800

    Add files via upload
    Buat Program Mengisi dan Menampilkan elemen Array dengan Index Array Sesuai dengan NIM Terakhir Setiap Mahasiswa

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+	int array[11];
+	printf("Nama : I GEDE RAKA NUGRAHA WIRAYUDHA \nNIM : F1B019064");
+	printf("\nKelompok : 13\n");
+	printf("Masukkan nilai 1 = ");
+	scanf("%d",&array[0]);
+	printf("Masukkan nilai 2 = ");
+	scanf("%d",&array[1]);
+	printf("Masukkan nilai 3 = ");
+	scanf("%d",&array[2]);
+	printf("Masukkan nilai 4 = ");
+	scanf("%d",&array[3]);
+	printf("Masukkan nilai 5 = ");
+	scanf("%d",&array[4]);
+	printf("Masukkan nilai 6 = ");
+	scanf("%d",&array[5]);
+	printf("Masukkan nilai 7 = ");
+	scanf("%d",&array[6]);
+	printf("Masukkan nilai 8 = ");
+	scanf("%d",&array[7]);
+	printf("Masukkan nilai 9 = ");
+	scanf("%d",&array[8]);
+	printf("Masukkan nilai 10 = ");
+	scanf("%d",&array[9]);
+	printf("Masukkan nilai 11 = ");
+	scanf("%d",&array[10]);
+	printf("\n\nData ke-1= %d",array[0]);
+	printf("\nData ke-2= %d",array[1]);
+	printf("\nData ke-3= %d",array[2]);
+	printf("\nData ke-4= %d",array[3]);
+	printf("\nData ke-5= %d",array[4]);
+	printf("\nData ke-6= %d",array[5]);
+	printf("\nData ke-7= %d",array[6]);
+	printf("\nData ke-8= %d",array[7]);
+	printf("\nData ke-9= %d",array[8]);
+	printf("\nData ke-10= %d",array[9]);
+	printf("\nData ke-11= %d",array[10]);
+}
+
