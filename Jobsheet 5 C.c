a37a7a757975e7436a12597666e426ec5385c0d6
dederaka <65152409+dederaka@users.noreply.github.com>
2020-05-11 21:15:58 +0800

    Add files via upload
    Buatlah program perkalian matriks dua dimensi dengan 2 nim mahasiswa

---
+++ Jobsheet 5 C.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+int m1[2][3],m2[2][3],i,j;
+int hasil[2][3];
+printf("Nama : I GEDE RAKA NUGRAHA WIRAYUDHA \nNIM : F1B019064\n");
+printf("\nMatriks 1\n");
+for (i = 0; i < 2; i++){
+for (j = 0; j < 3; j++){
+printf ("Masukkan nilai  %d, %d = ", i, j);
+scanf ("%d", &m1[i][j]);
+}
+}
+printf ("\n");
+printf("Matriks 2\n");
+for (i = 0; i < 2; i++){
+for (j = 0; j < 3; j++){
+printf ("Masukkan nilai  %d, %d = ", i, j);
+scanf ("%d", &m2[i][j]);}}
+printf ("\nHasil Perkalian \n");
+for (i = 0; i < 2; i++){
+for (j = 0; j < 3;j++){
+hasil [i][j]=m1[i][j]*m2[i][j];
+printf ("%d", hasil[i][j]);
+printf(" ");
+}
+printf ("\n");
+}
+getch();
+}
+
