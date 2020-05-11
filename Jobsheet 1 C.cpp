06e80a3a56a2c780621f85d84f7a9714ff735a5c
dederaka <65152409+dederaka@users.noreply.github.com>
2020-05-11 21:10:40 +0800

    Add files via upload
    Buatlah Program Array Satu Dimensi Menampilkan NIM Masing-masing Mahasiswa

---
+++ Jobsheet 1 C.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+main()
+{
+char* nim_ray[13]={"01","02","03","04","05","06","07","08","09","10","11","12","13"};
+	printf("F1B019064 - 1 = %s\n",nim_ray[0]);
+	printf("F1B019064 ke- 2 = %s\n",nim_ray[1]);
+	printf("F1B019064 ke- 3 = %s\n",nim_ray[2]);
+	printf("F1B019064 ke- 4 = %s\n",nim_ray[3]);
+	printf("F1B019064 ke- 5 = %s\n",nim_ray[4]);
+	printf("F1B019064 ke- 6 = %s\n",nim_ray[5]);
+	printf("F1B019064 ke- 7 = %s\n",nim_ray[6]);
+	printf("F1B019064 ke- 8 = %s\n",nim_ray[7]);
+	printf("F1B019064 ke- 9 = %s\n",nim_ray[8]);
+	printf("F1B019064 ke- 10 = %s\n",nim_ray[9]);
+	printf("F1B019064 ke- 11 = %s\n",nim_ray[10]);
+	printf("F1B019064 ke- 12 = %s\n",nim_ray[11]);
+	printf("F1B019064 ke- 13 = %s\n",nim_ray[12]);
+	
+}
+
