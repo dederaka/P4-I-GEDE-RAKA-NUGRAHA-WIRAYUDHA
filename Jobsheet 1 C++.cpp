157d1bf33ff20f15bb0459f2e3e4f6b8940b2f0a
dederaka <65152409+dederaka@users.noreply.github.com>
2020-05-11 21:11:15 +0800

    Add files via upload
    Buatlah Program Array Satu Dimensi Menampilkan NIM Masing-masing Mahasiswa

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+main (){
+char* my_array[13]={"01","02","03","04","05","06","07","08","09","10","11","12","13"};
+	
+	cout<<"F1B019064 ke- 1 = "<<my_array[0]<<"\n";
+	cout<<"F1B019064 ke- 2 = "<<my_array[1]<<"\n";
+	cout<<"F1B019064 ke- 3 = "<<my_array[2]<<"\n";
+	cout<<"F1B019064 ke- 4 = "<<my_array[3]<<"\n";
+	cout<<"F1B019064 ke- 5 = "<<my_array[4]<<"\n";
+	cout<<"F1B019064 ke- 6 = "<<my_array[5]<<"\n";
+	cout<<"F1B019064 ke- 7 = "<<my_array[6]<<"\n";
+	cout<<"F1B019064 ke- 8 = "<<my_array[7]<<"\n";
+	cout<<"F1B019064 ke- 9 = "<<my_array[8]<<"\n";
+	cout<<"F1B019064 ke- 10 = "<<my_array[9]<<"\n";
+	cout<<"F1B019064 ke- 11 = "<<my_array[10]<<"\n";
+	cout<<"F1B019064 ke- 12 = "<<my_array[11]<<"\n";
+	cout<<"F1B019064 ke- 13 = "<<my_array[12]<<"\n";
+
+}
+
